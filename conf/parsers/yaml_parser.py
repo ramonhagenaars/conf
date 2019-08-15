fe99c4de08109b8f8816c7adc9b0bdd39ed82676
Gijs Molenaar <gijs@pythonic.nl>
2019-08-15 16:04:14 +0200

    be more backwards compatible

--- conf/parsers/yaml_parser.py
+++ conf/parsers/yaml_parser.py
@@ -5,8 +5,16 @@ except ImportError:
 
 
 def parse(file_stream):
-    # Parse the given file stream of yaml format to a dict.
-    loaded = yaml.load(file_stream, Loader=yaml.FullLoader)
+    """
+    Parse the given file stream of yaml format to a dict.
+    """
+    major, minor = [int(i) for i in yaml.__version__.split('.')][0:2]
+
+    if (major == 5 and minor >= 1) or (major > 5):
+        loaded = yaml.load(file_stream, Loader=yaml.FullLoader)
+    else:
+        loaded = yaml.load(file_stream)
+
     if loaded:
         return dict(loaded.items())
     return {}
